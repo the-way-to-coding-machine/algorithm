54fd752d2808f36345bf27c749eb13678a05faa7
sonjongyoung <sondahum@gmail.com>
2020-08-06 10:52:27 +0900

    solve leetcode 1295

--- src/org/wtcm/leetcode/q1295/FindEvenNumberOfDigits_JY.java
+++ src/org/wtcm/leetcode/q1295/FindEvenNumberOfDigits_JY.java
@@ -2,6 +2,21 @@ package org.wtcm.leetcode.q1295;
 
 public class FindEvenNumberOfDigits_JY {
     public int findNumbers(int[] nums) {
-        return 0;
+        int count = 0;
+
+        for (int number : nums) {
+            if (countDigit(number) % 2 == 0)
+                ++count;
+        }
+        return count;
+    }
+
+    public int countDigit(int num) {
+        int cnt = 0;
+
+        for (int i = num; num != 0; num /= 10) {
+            ++cnt;
+        }
+        return cnt;
     }
 }
---
+++ src/org/wtcm/leetcode/q1295/Runner.java
@@ -0,0 +1,11 @@
+package org.wtcm.leetcode.q1295;
+
+public class Runner {
+    public static void main(String[] args) {
+        FindEvenNumberOfDigits_JY question = new FindEvenNumberOfDigits_JY();
+
+        int[] input = {12,345,2,6,7896};
+
+        System.out.println(question.findNumbers(input));
+    }
+}
