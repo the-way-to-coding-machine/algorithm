53f56e5e1cc91f02b12e723ff7d667e3262105d3
sonjongyoung <sondahum@gmail.com>
2020-08-06 17:00:49 +0900

    solve leetcode 1312

---
+++ src/org/wtcm/leetcode/q1312/MakePalindrome.java
@@ -0,0 +1,74 @@
+package org.wtcm.leetcode.q1312;
+
+public class MakePalindrome {
+    //    // mbadm
+//    // leetcode
+//    /* note. it doesn't need to make real palindrome.
+//        first, find the longest palindrome sub-sequence and make another palindrome with the rest characters.
+//        the point is to find 'longest sub-sequence'..!!
+//     */
+//    public int minInsertions(String s) {
+//        int cnt = 0;
+//
+//        if (s.length() % 2 == 0) {
+//            s += "A";
+//        }
+//
+//        char[] string = s.toCharArray();
+//        int center = 0;
+//
+//        center = string.length / 2;
+//        for (int first = 0; first < center; first++) {
+//            if (string[first] != string[string.length - 1 - first])
+//                ++cnt;
+//        }
+//        cnt *=2;
+//        return s.endsWith("A") ? cnt-1 : cnt;
+//    }
+    Integer[][] memo;
+
+    public int minInsertions(String s) {
+        memo = new Integer[s.length()][s.length()];
+        char[] arr = s.toCharArray();
+        return help(arr, 0, s.length() - 1);
+    }
+
+    int help(char[] arr, int l, int r) {
+        if (l >= r)
+            return 0;
+        if (memo[l][r] != null)
+            return memo[l][r];
+
+        int count = 0;
+
+        if (arr[l] == arr[r]) {
+            count = help(arr, l + 1, r - 1);
+        } else {
+            count = Math.min(help(arr, l + 1, r), help(arr, l, r - 1)) + 1;
+        }
+        memo[l][r] = count;
+        return count;
+    }
+
+    public int minInsertions1(String s) {
+        final int n = s.length();
+        int[] curr = new int[n];
+        int[] pre = new int[n];
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j < n; j++) {
+                if (i == j) {
+                    curr[j] = 0;
+                    continue;
+                }
+                if (s.charAt(i) == s.charAt(j))
+                    curr[j] = pre[j - 1];
+                else curr[j] = 1 + Math.min(pre[j], curr[j - 1]);
+            }
+            int[] t = pre;
+            pre = curr;
+            curr = t;
+        }
+        return pre[n - 1];
+    }
+}
---
+++ src/org/wtcm/leetcode/q1312/Runner.java
@@ -0,0 +1,14 @@
+package org.wtcm.leetcode.q1312;
+
+public class Runner {
+    public static void main(String[] args) {
+        MakePalindrome question = new MakePalindrome();
+//        String input = "mbadm";
+//        String input = "zzazz";
+//        String input = "nom";
+//        String input = "leetcode";
+        String input = "zjveiiwvc";
+
+        System.out.println(question.minInsertions(input));
+    }
+}
