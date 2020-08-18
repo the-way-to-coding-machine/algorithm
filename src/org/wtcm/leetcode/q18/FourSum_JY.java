c55db9430cb1036e211366d4114be8f7c12fd3f1
sonjongyoung <sondahum@gmail.com>
2020-08-18 17:06:10 +0900

    solve leetcode 18 (//TODOnot my solution..)

--- src/org/wtcm/leetcode/q18/FourSum_JY.java
+++ src/org/wtcm/leetcode/q18/FourSum_JY.java
@@ -13,12 +13,15 @@ public class FourSum_JY {
         Arrays.sort(nums);
         return kSum(nums, target, 0, 4);
     }
+
     public List<List<Integer>> kSum(int[] nums, int target, int start, int k) {
         List<List<Integer>> res = new ArrayList<>();
         if (start == nums.length || nums[start] * k > target || target > nums[nums.length - 1] * k)
             return res;
+
         if (k == 2)
             return twoSum(nums, target, start);
+
         for (int i = start; i < nums.length; ++i)
             if (i == start || nums[i - 1] != nums[i])
                 for (List<Integer> set : kSum(nums, target - nums[i], i + 1, k - 1)) {
@@ -27,6 +30,7 @@ public class FourSum_JY {
                 }
         return res;
     }
+
     public List<List<Integer>> twoSum(int[] nums, int target, int start) {
         List<List<Integer>> res = new ArrayList<>();
         int lo = start, hi = nums.length - 1;
--- src/org/wtcm/leetcode/q18/Runner.java
+++ src/org/wtcm/leetcode/q18/Runner.java
@@ -4,8 +4,8 @@ public class Runner {
     public static void main(String[] args) {
         FourSum_JY question = new FourSum_JY();
 
-//        int[] nums = {1,0,-1,0,-2,2};
-        int[] nums = {0,0,0,0};
+        int[] nums = {1,0,-1,0,-2,2};
+//        int[] nums = {0,0,0,0};
         int target = 0;
 
         question.fourSum(nums, target).forEach(System.out::println);
