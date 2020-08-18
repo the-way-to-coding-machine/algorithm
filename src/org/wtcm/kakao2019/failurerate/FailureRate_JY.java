6a0c29ccfa48d463cee3dc2804fceadc3a11eb86
sonjongyoung <sondahum@gmail.com>
2020-08-18 17:58:26 +0900

    resolving kakao2019 failure rate problem

--- src/org/wtcm/kakao2019/failurerate/FailureRate_JY.java
+++ src/org/wtcm/kakao2019/failurerate/FailureRate_JY.java
@@ -4,45 +4,19 @@ import java.util.Arrays;
 
 public class FailureRate_JY {
     public int[] solution(int N, int[] stages) {
-        int[] answer = new int[N];
-        int[] failuredPlayer = new int[N+2];
-
-        for (int stage : stages){
-            failuredPlayer[stage]++;
+        int[] cache = new int[N+2]; // cache[i] == stage i 에 있는 사람 수.
+        double[] result = new double[N+1];
+        for (int i : stages) {
+            cache[i]++;
         }
-        int total = stages.length;
-        Record[] records = new Record[N];
-        records[0] = new Record(0,0);
-        for (int stage = 1; stage <= N; stage++) {
-            if (total != 0) {
-                records[stage-1] = new Record(stage, (double)failuredPlayer[stage] / total);
-                total -= failuredPlayer[stage];
-            }
-            else {
-                records[stage-1] = new Record(0,0);
-            }
+
+        for (int failure = 1; failure <= N; failure++) {
+            int upper = cache[failure];
+            double lower = 0;
+            for (int s = failure; s <= N+1; s++)
+                lower += cache[s];
+            result[failure] = upper/lower;
         }
-        Arrays.sort(records);
-        for (int idx = 0; idx < N; idx++) {
-            answer[idx] = records[idx].stage;
-        }
-        return answer;
-    }
-}
-class Record implements Comparable<Record> {
-    int stage;
-    double failureRate;
-
-    Record(int stage, double failureRate) {
-        this.stage = stage;
-        this.failureRate = failureRate;
-    }
-
-    @Override
-    public int compareTo(Record o) {
-        if (this.failureRate == o.failureRate)
-            return this.stage - o.stage;
-
-        return Double.compare(o.failureRate, this.failureRate);
+        return null;
     }
 }
---
+++ src/org/wtcm/kakao2019/failurerate/Runner.java
@@ -0,0 +1,17 @@
+package org.wtcm.kakao2019.failurerate;
+
+import java.util.Arrays;
+
+public class Runner {
+    public static void main(String[] args) {
+        FailureRate_JY question = new FailureRate_JY();
+
+//        int N = 5;
+//        int[] stages = {2,1,2,6,2,4,3,3};
+
+        int N = 4;
+        int[] stages = {4,4,4,4,4};
+
+        Arrays.stream(question.solution(N,stages)).forEach(System.out::println);
+    }
+}
