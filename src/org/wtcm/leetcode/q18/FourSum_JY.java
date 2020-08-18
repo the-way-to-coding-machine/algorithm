fc1bbc8e7b8b308b8e6242405fc3ba9344cfce13
sonjongyoung <sondahum@gmail.com>
2020-08-18 15:06:04 +0900

    solve leetcode 18

--- src/org/wtcm/leetcode/q18/FourSum_JY.java
+++ src/org/wtcm/leetcode/q18/FourSum_JY.java
@@ -1,4 +1,44 @@
 package org.wtcm.leetcode.q18;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+/*  note. 여기서부터는 이제 k-sum으로 가야한다.
+        idea는 k-sum을 쪼개고 쪼개서 2sum을 이용한다는 것!
+* */
 public class FourSum_JY {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        return kSum(nums, target, 0, 4);
+    }
+    public List<List<Integer>> kSum(int[] nums, int target, int start, int k) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (start == nums.length || nums[start] * k > target || target > nums[nums.length - 1] * k)
+            return res;
+        if (k == 2)
+            return twoSum(nums, target, start);
+        for (int i = start; i < nums.length; ++i)
+            if (i == start || nums[i - 1] != nums[i])
+                for (List<Integer> set : kSum(nums, target - nums[i], i + 1, k - 1)) {
+                    res.add(new ArrayList<>(Arrays.asList(nums[i])));
+                    res.get(res.size() - 1).addAll(set);
+                }
+        return res;
+    }
+    public List<List<Integer>> twoSum(int[] nums, int target, int start) {
+        List<List<Integer>> res = new ArrayList<>();
+        int lo = start, hi = nums.length - 1;
+        while (lo < hi) {
+            int sum = nums[lo] + nums[hi];
+            if (sum < target || (lo > start && nums[lo] == nums[lo - 1]))
+                ++lo;
+            else if (sum > target || (hi < nums.length - 1 && nums[hi] == nums[hi + 1]))
+                --hi;
+            else
+                res.add(Arrays.asList(nums[lo++], nums[hi--]));
+        }
+        return res;
+    }
 }
--- src/org/wtcm/leetcode/q454/Runner.java
+++ src/org/wtcm/leetcode/q454/Runner.java
@@ -4,10 +4,15 @@ public class Runner {
     public static void main(String[] args) {
         FourSum2_JY question = new FourSum2_JY();
 
-        int[] A = {0};
-        int[] B = {0};
-        int[] C = {0};
-        int[] D = {0};
+        int[] A = {1,2};
+        int[] B = {-2,-1};
+        int[] C = {-1,2};
+        int[] D = {0,2};
+
+//        int[] A = {0};
+//        int[] B = {0};
+//        int[] C = {0};
+//        int[] D = {0};
 
         System.out.println(question.fourSumCount(A,B,C,D));
     }
