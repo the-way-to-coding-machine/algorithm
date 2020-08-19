b375d7aa69f634d03b40ec73517a5245c785cf53
sonjongyoung <sondahum@gmail.com>
2020-08-19 13:15:26 +0900

    solve kakao2019 failure rate problem

--- src/org/wtcm/kakao2019/failurerate/FailureRate_JY.java
+++ src/org/wtcm/kakao2019/failurerate/FailureRate_JY.java
@@ -1,6 +1,7 @@
 package org.wtcm.kakao2019.failurerate;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class FailureRate_JY {
     public int[] solution(int N, int[] stages) {
@@ -15,8 +16,13 @@ public class FailureRate_JY {
             double lower = 0;
             for (int s = failure; s <= N+1; s++)
                 lower += cache[s];
+
+            lower = lower == 0 ? -1 : lower; // note. 코너케이스를 잘 생각하자..!
             result[failure] = upper/lower;
         }
-        return null;
+
+        // note. StackOverflow에서 본 stream팁..!! 숙지하기1!
+        return IntStream.range(1, result.length).boxed().sorted((i,j) -> Double.compare(result[j], result[i]))
+                .mapToInt(ele -> ele).toArray();
     }
 }
