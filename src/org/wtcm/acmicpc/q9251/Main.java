bd18c7b615daafc09561b8e626f3369b49738d65
sonjongyoung <sondahum@gmail.com>
2020-08-24 17:54:30 +0900

    solve acmicpc 9251

--- src/org/wtcm/acmicpc/q9251/Main.java
+++ src/org/wtcm/acmicpc/q9251/Main.java
@@ -1,13 +1,24 @@
 package org.wtcm.acmicpc.q9251;
 
-public class Runner {
-    public static void main(String[] args) {
-        LongestCommonSubsequence question = new LongestCommonSubsequence();
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-        String str1 = "ACAYKP";
-        String str2 = "CAPCAK";
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        System.out.println(question.retry(str1, str2));
+        String str1 = br.readLine();
+        String str2 = br.readLine();
+        int[][] dp = new int[str1.length()+1][str2.length()+1];
 
+        for (int idx1 = 1; idx1 <= str1.length(); idx1++)
+            for (int idx2 = 1; idx2 <= str2.length(); idx2++)
+                if (str1.charAt(idx1-1) == str2.charAt(idx2-1))
+                    dp[idx1][idx2] = dp[idx1-1][idx2-1] + 1;
+                else
+                    dp[idx1][idx2] = Math.max(dp[idx1][idx2-1], dp[idx1-1][idx2]);
+
+        System.out.println(dp[str1.length()][str2.length()]);
     }
 }
