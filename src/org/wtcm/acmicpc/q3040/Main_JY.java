23e01cc5df4abf12f844d76632b0ce95e80555e0
sonjongyoung <sondahum@gmail.com>
2020-08-27 15:18:54 +0900

    add JY to acmicpc files

--- src/org/wtcm/acmicpc/q3040/Main_JY.java
+++ src/org/wtcm/acmicpc/q3040/Main_JY.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.stream.IntStream;
 
-public class Main {
+public class Main_JY {
 
     public static void main(String[] args) throws IOException {
        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
