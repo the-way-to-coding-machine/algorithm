7813d0ecbc2d80707ad3ed6d305ddae2fa12111c
sonjongyoung <sondahum@gmail.com>
2020-08-28 17:30:23 +0900

    add acmicpc 2315

---
+++ src/org/wtcm/acmicpc/q2315/Main_JY.java
@@ -0,0 +1,11 @@
+package org.wtcm.acmicpc.q2315;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_JY {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
