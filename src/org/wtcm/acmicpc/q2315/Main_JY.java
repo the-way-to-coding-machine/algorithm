59e434267b8d17560244b94aaee9851f103b1d1d
sonjongyoung <sondahum@gmail.com>
2020-09-01 10:32:51 +0900

    solve acmicpc 2315

--- src/org/wtcm/acmicpc/q2315/Main_JY.java
+++ src/org/wtcm/acmicpc/q2315/Main_JY.java
@@ -18,7 +18,7 @@ public class Main_JY {
 
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken()); // 가로등 갯수
-        M = Integer.parseInt(st.nextToken()) - 1; // 처음 위치
+        M = Integer.parseInt(st.nextToken()); // 처음 위치
 
         D = new int[N + 1];
         W = new int[N + 1];
@@ -33,31 +33,33 @@ public class Main_JY {
 
         }
 
-//        for (int i = 0; i < N + 1; i++) {
-//            for (int j = 0; j < N + 1; j++) {
-//                Arrays.fill(dp[i][j], -1);
+        for (int i = 0; i < N + 1; i++)
+            for (int j = 0; j < N + 1; j++)
+                Arrays.fill(dp[i][j], -1);
+
+
+//        for (int start = 1; start <= N; start++) {
+//            for (int end = 1; end <= N; end++) {
+//                int outer = W_ACC[N] - W_ACC[end] + W_ACC[start];
+//                if (start < N)
+//                    dp[start][end][LEFT] = Math
+//                            .min(
+//                                    dp[start + 1][end][LEFT] + (D[start + 1] - D[start]) * outer,
+//                                    dp[start + 1][end][RIGHT] + (D[end] - D[start]) * outer
+//                            );
+//                if (end > 1)
+//                    dp[start][end][RIGHT] = Math
+//                            .min(
+//                                    dp[start][end - 1][LEFT] + (D[end] - D[start]) * outer,
+//                                    dp[start][end - 1][RIGHT] + (D[end] - D[end - 1]) * outer
+//                            );
 //            }
 //        }
-        for (int start = 1; start < N; start++) {
-            for (int end = start + 1; end <= N; end++) {
-                int outer = W_ACC[N]-W_ACC[end]+W_ACC[start-1];
-                dp[start][end][LEFT] = Math
-                        .min(
-                                dp[start + 1][end][LEFT]+(D[start+1]-D[start])*outer,
-                                dp[start + 1][end][RIGHT]+(D[end]-D[start])*outer
-                        );
-                dp[start][end][RIGHT] = Math
-                        .min(
-                                dp[start][end - 1][LEFT]+(D[end]-D[start])*outer,
-                                dp[start][end - 1][RIGHT]+(D[end]-D[end-1])*outer
-                        );
-            }
-        }
-        System.out.println(Math.min(dp[1][N][LEFT], dp[1][N][RIGHT]));
-//        System.out.println(move(M, M, 0));
+//        System.out.println(Math.min(dp[1][N][LEFT], dp[1][N][RIGHT]));
+        System.out.println(turnOff(M, M, 0));
     }
 
-    public static int move(int start, int end, int position) {
+    public static int turnOff(int start, int end, int position) {
         if (start == 1 && end == N) {
             return 0;
         }
@@ -68,10 +70,10 @@ public class Main_JY {
         int current = position == LEFT ? start : end;
         int outer = W_ACC[N] - W_ACC[end] + W_ACC[start - 1];
         if (start - 1 >= 1) {
-            left = move(start - 1, end, LEFT) + (D[current] - D[start - 1]) * outer;
+            left = turnOff(start - 1, end, LEFT) + (D[current] - D[start - 1]) * outer;
         }
         if (end + 1 <= N) {
-            right = move(start, end + 1, RIGHT) + (D[end + 1] - D[current]) * outer;
+            right = turnOff(start, end + 1, RIGHT) + (D[end + 1] - D[current]) * outer;
         }
         return dp[start][end][position] = Math.min(left, right);
     }
