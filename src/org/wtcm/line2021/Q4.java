d4357a99e280342cde7d84d81ab6466dafd2c04f
sonjongyoung <sondahum@gmail.com>
2020-09-14 18:10:06 +0900

    trying to solve line 2021 problem 4

--- src/org/wtcm/line2021/Q4.java
+++ src/org/wtcm/line2021/Q4.java
@@ -1,16 +1,14 @@
 package org.wtcm.line2021;
 
+
 import java.util.Arrays;
 
 public class Q4 {
-    static int[] xMove = {0, 1, -1, 0}; // 밑  왼 위
-    static int[] yMove = {1, 0, 0, -1};
-    static boolean[][] visited;
-    static int length;
-    static int N, M;
+    static int[] rowDir = {0, 1, 0, -1}; // 오 밑 왼 위
+    static int[] colDir = {1, 0, -1, 0};
+    static int width, height, length = Integer.MAX_VALUE;
     static int[][] map;
-    static int[] wallX = {1, 0, 0, -1}; // 오 위 밑 위
-    static int[] wallY = {0, -1, 1, 0};
+    static boolean[][] visited;
 
     public static void main(String[] args) {
         int[][] maze = {
@@ -30,38 +28,73 @@ public class Q4 {
     }
 
     static int solution(int[][] maze) {
-        M = maze.length;
-        N = maze[0].length;
-        visited = new boolean[M][N];
+        height = maze.length;
+        width = maze[0].length;
         map = maze;
 
-        for (int i = 0; i < M; i++)
+        visited = new boolean[height][width];
+        for (int i = 0; i < height; i++)
             Arrays.fill(visited[i], false);
 
-        length = 0;
-        dfs(0, 0);
+
+//        simpleDFS(0, 0, 0);
+        leftHandRule(0,0,0);
+
         return length;
     }
 
-    static void dfs(int y, int x) {
-        if (x == N - 1 && y == M - 1) {
+    static void leftHandRule(int row, int col, int move) {
+        if (row == height - 1 && col == width - 1) {
+            length = Math.min(length, move);
             return;
         }
-        length++;
-        for (int direction = 0; direction < 4; direction++) {
-            int wallPosX = x + wallX[direction];
-            int wallPosY = y + wallY[direction];
 
-            if (wallPosY >= N || wallPosY < 0 || wallPosX >= M || wallPosX < 0) continue;
-            if (map[wallPosY][wallPosX] != 1) continue;
+        /*
+         * 1. 좌벽 유무 확인
+         *   --> 없으면 왼쪽으로
+         *   --> 있으면 왼쪽으로 회전
+         * 2.
+         * */
+        for (int turn = 0; turn < 4; turn++) {
+            int nextRow = row + rowDir[turn];
+            int nextCol = col + colDir[turn];
 
-            int nextX = x + xMove[direction];
-            int nextY = y + yMove[direction];
+            int currentLeftRow = row + rowDir[(turn+3)%4];
+            int currentLeftCol = col + colDir[(turn+3)%4];
 
-            if (nextX >= N || nextX < 0 || nextY >= M || nextY < 0) continue; // 구석
-            if (map[nextY][nextX] != 0) continue; // 벽
+            if (!isIn(currentLeftRow, currentLeftCol)) continue;
 
-            dfs(nextY, nextX);
+            if (!isIn(nextRow, nextCol)) continue;
+            if (map[nextRow][nextCol] != 0) continue;
+
+            leftHandRule(nextRow, nextCol, move+1);
         }
+    }
+
+
+
+//    static void simpleDFS(int row, int col, int cur) {
+//        if (row == height - 1 && col == width - 1) {// 도착
+//            length = Math.min(length,cur);
+//            return;
+//        }
+//
+//        if (!visited[row][col]) {
+//            visited[row][col] = true;
+//            for (int dir = 0; dir < 4; dir++) {
+//                int nextRow = row + rowDir[dir];
+//                int nextCol = col + colDir[dir];
+//
+//                if (!isIn(nextRow, nextCol)) continue;
+//                if (map[nextRow][nextCol] != 0) continue;
+//
+//                simpleDFS(nextRow, nextCol, cur + 1);
+//            }
+//                visited[row][col] = false;
+//        }
+//    }
+
+    static boolean isIn(int row, int col) {
+        return row < height && row >= 0 && col < width && col >= 0;
     }
 }
