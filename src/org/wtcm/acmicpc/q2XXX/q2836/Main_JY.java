4902401614ecfc257d80ab52b1209ed78c245f20
sonjongyoung <sondahum@gmail.com>
2020-09-21 17:50:58 +0900

    solve acmicpc 2836 problems

--- src/org/wtcm/acmicpc/q2XXX/q2836/Main_JY.java
+++ src/org/wtcm/acmicpc/q2XXX/q2836/Main_JY.java
@@ -1,12 +1,184 @@
 package org.wtcm.acmicpc.q2XXX.q2836;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
 
 public class Main_JY {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+        InputReader in = new InputReader(System.in);
+        OutputWriter out = new OutputWriter(System.out);
+
+        Task question = new Task();
+        question.solution(in, out);
+        out.close();
+    }
+}
+
+class Task {
+    long N, M;
+    List<Path> paths;
+
+    void solution(InputReader in, OutputWriter out) {
+        N = in.nextInt();
+        M = in.nextInt();
+        paths = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            int[] input = in.nextIntArray(2);
+            if (input[1] < input[0])
+                paths.add(new Path(input));
+        }
+        paths.sort((p1, p2) -> p1.start - p2.start);
+
+        long ans = M;
+        long length = 0;
+        long start=0, end=0;
+        for (int i = 0; i < paths.size(); i++) {
+            Path cur = paths.get(i);
+
+            if (cur.start <= end) {
+                end = Math.max(cur.start, end);
+            } else {
+                length += (end - start);
+                start = cur.start;
+                end = cur.end;
+            }
+        }
+        length += end-start;
+        out.print(ans+2*length);
+    }
+
+    private static class Path {
+        int start;
+        int end;
+
+        Path(int[] se) {
+            start = se[1];
+            end = se[0];
+        }
+    }
+}
+
+class InputReader {
+    private InputStream stream;
+    private byte[] buf = new byte[1024];
+    private int curChar;
+    private int numChars;
+
+    public int read() {
+        if (numChars == -1) {
+            throw new InputMismatchException();
+        } else {
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException var2) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0) {
+                    return -1;
+                }
+            }
+            return buf[curChar++];
+        }
+    }
+
+    public boolean isSpaceChar(int c) {
+        return c == 32 || c == 10 || c == 13 || c == 9 || c == -1;
+    }
+
+    public InputReader(InputStream stream) {
+        this.stream = stream;
+    }
+
+    public String next() {
+        int c = this.read();
+        while (isSpaceChar(c)) {
+            c = this.read();
+        }
+        StringBuilder result = new StringBuilder();
+        result.appendCodePoint(c);
+        while (!isSpaceChar(c = this.read())) {
+            result.appendCodePoint(c);
+        }
+        return result.toString();
+    }
+
+    public int nextInt() {
+        int c = this.read();
+        while (isSpaceChar(c)) {
+            c = this.read();
+        }
+        byte sgn = 1;
+        if (c == 45) {
+            sgn = -1;
+            c = this.read();
+        }
+        int res = 0;
+        while (c >= 48 && c <= 57) {
+            res *= 10;
+            res += c - 48;
+            c = this.read();
+            if (isSpaceChar(c)) {
+                return res * sgn;
+            }
+        }
+        throw new InputMismatchException();
+    }
+
+    public String[] nextArray(int size) {
+        String[] ret = new String[size];
+        for (int i = 0; i < size; i++) {
+            ret[i] = this.next();
+        }
+        return ret;
+    }
+
+    public int[] nextIntArray(int size) {
+        int[] ret = new int[size];
+        for (int i = 0; i < size; i++) {
+            ret[i] = this.nextInt();
+        }
+        return ret;
+    }
+}
+
+class OutputWriter {
+    private final PrintWriter writer;
+
+    public OutputWriter(OutputStream outputStream) {
+        writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+    }
+
+    public OutputWriter(Writer writer) {
+        this.writer = new PrintWriter(writer);
+    }
+
+    public void print(int i) {
+        writer.print(i);
+    }
+
+    public void print(long i) {
+        writer.print(i);
+    }
+
+    public void print(Object... objects) {
+        int len = objects.length;
+        for (int i = 0; i < len; i++) {
+            if (i != 0) {
+                writer.print(' ');
+            }
+            writer.print(objects[i]);
+        }
+    }
+
+    public void println() {
+        writer.println();
+    }
+
+    public void close() {
+        writer.close();
     }
 }
