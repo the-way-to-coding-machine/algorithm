759e5644bb380790f9cd77ebf3925878539256bc
sondahum <sondahum@gmail.com>
2020-09-24 06:01:15 +0900

    resolved acmicpc 2315 problem

--- src/org/wtcm/acmicpc/q2XXX/q2315/Main_JY.java
+++ src/org/wtcm/acmicpc/q2XXX/q2315/Main_JY.java
@@ -7,59 +7,78 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main_JY {
+
+    public static void main(String[] args) throws IOException {
+        Task question = new Task();
+
+//        question.loop();
+        question.recursive();
+    }
+}
+
+class Task {
     static int N, M;
-    static int[] D, W, W_ACC;
+    static int[] D, W, sum;
     static StringTokenizer st;
     static int[][][] dp;
     static final int LEFT = 0, RIGHT = 1;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    Task() throws IOException {
+        init();
+    }
 
+    void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken()); // 가로등 갯수
         M = Integer.parseInt(st.nextToken()); // 처음 위치
 
         D = new int[N + 1];
         W = new int[N + 1];
-        W_ACC = new int[N + 1];
+        sum = new int[N + 1];
         dp = new int[N + 1][N + 1][2];
 
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
             D[i] = Integer.parseInt(st.nextToken());
             W[i] = Integer.parseInt(st.nextToken());
-            W_ACC[i] = W_ACC[i - 1] + W[i]; // 소비전력의 누적합
-
+            sum[i] = sum[i - 1] + W[i]; // 소비전력의 누적합
         }
 
-        for (int i = 0; i < N + 1; i++)
-            for (int j = 0; j < N + 1; j++)
-                Arrays.fill(dp[i][j], -1);
+        for (int i = 1; i <= N; i++)
+            for (int j = 1; j <= N; j++)
+                Arrays.fill(dp[i][j], 1000000000);
 
-
-//        for (int start = 1; start <= N; start++) {
-//            for (int end = 1; end <= N; end++) {
-//                int outer = W_ACC[N] - W_ACC[end] + W_ACC[start];
-//                if (start < N)
-//                    dp[start][end][LEFT] = Math
-//                            .min(
-//                                    dp[start + 1][end][LEFT] + (D[start + 1] - D[start]) * outer,
-//                                    dp[start + 1][end][RIGHT] + (D[end] - D[start]) * outer
-//                            );
-//                if (end > 1)
-//                    dp[start][end][RIGHT] = Math
-//                            .min(
-//                                    dp[start][end - 1][LEFT] + (D[end] - D[start]) * outer,
-//                                    dp[start][end - 1][RIGHT] + (D[end] - D[end - 1]) * outer
-//                            );
-//            }
-//        }
-//        System.out.println(Math.min(dp[1][N][LEFT], dp[1][N][RIGHT]));
-        System.out.println(turnOff(M, M, 0));
+        dp[M][M][LEFT] = dp[M][M][RIGHT] = 0;
     }
 
-    public static int turnOff(int start, int end, int position) {
+    void loop() {
+        for (int distance = 2; distance <= N; distance++) {
+            for (int start = 1; start <= N - distance + 1; start++) {
+                int end = start + distance - 1;
+                int outer1 = sum[N] - sum[end] + sum[start], outer2 = sum[N] - sum[end - 1] + sum[start - 1];
+
+                dp[start][end][LEFT] = Math.min(
+                        dp[start + 1][end][LEFT] + (D[start + 1] - D[start]) * outer1
+                        , dp[start + 1][end][RIGHT] + (D[end] - D[start]) * outer1);
+                dp[start][end][RIGHT] = Math.min(
+                        dp[start][end - 1][RIGHT] + (D[end] - D[end - 1]) * outer2
+                        , dp[start][end - 1][LEFT] + (D[end] - D[start]) * outer2);
+            }
+        }
+        int min = Math.min(dp[1][N][LEFT], dp[1][N][RIGHT]);
+        System.out.println(min);
+    }
+
+    void recursive() {
+        for (int i = 1; i <= N; i++)
+            for (int j = 1; j <= N; j++)
+                Arrays.fill(dp[i][j], -1);
+
+        System.out.println(doRecursive(M,M,0));
+    }
+
+    int doRecursive(int start, int end, int position) {
         if (start == 1 && end == N) {
             return 0;
         }
@@ -68,12 +87,12 @@ public class Main_JY {
         int left, right;
         left = right = Integer.MAX_VALUE;
         int current = position == LEFT ? start : end;
-        int outer = W_ACC[N] - W_ACC[end] + W_ACC[start - 1];
+        int outer = sum[N] - sum[end] + sum[start - 1];
         if (start - 1 >= 1) {
-            left = turnOff(start - 1, end, LEFT) + (D[current] - D[start - 1]) * outer;
+            left = doRecursive(start - 1, end, LEFT) + (D[current] - D[start - 1]) * outer;
         }
         if (end + 1 <= N) {
-            right = turnOff(start, end + 1, RIGHT) + (D[end + 1] - D[current]) * outer;
+            right = doRecursive(start, end + 1, RIGHT) + (D[end + 1] - D[current]) * outer;
         }
         return dp[start][end][position] = Math.min(left, right);
     }
