9be90b6c6f42dafb809ac3545e60b74506aefa7c
sonjongyoung <sondahum@gmail.com>
2020-10-06 14:52:25 +0900

    fix combination practice code

--- src/org/wtcm/practice/combination/Combination.java
+++ src/org/wtcm/practice/combination/Combination.java
@@ -1,28 +1,29 @@
 package org.wtcm.practice.combination;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Combination {
-    private int[] array;
+    private int[] originArr;
     private int size;
     private List<List<Integer>> total;
     private boolean[] picked;
 
-    public Combination(int[] array) {
-        this.array = array;
-        this.size = array.length;
+    public Combination(int[] originArr) {
+        this.originArr = originArr;
+        this.size = originArr.length;
     }
 
     public List<List<Integer>> pick(int num, boolean duplicate) {
-        total = new ArrayList<>();
-        picked = new boolean[size];
-//        innerPick(0, num, picked);
-        int[] recomb = new int[num];
-        recomb(recomb, size, num, 0, 0);
-        return total;
+        this.total = new ArrayList<>();
+        int[] result = new int[num];
+        if (duplicate) duplicateCombination(result, size, num, 0, 0);
+        else combination(result, size, num, 0, 0);
+
+        return this.total;
     }
 
     public int[][] pick(int num, int sum, boolean duplicate) {
@@ -50,23 +51,32 @@ public class Combination {
         List<Integer> pair = new ArrayList<>();
         for (int idx = 0; idx < size; idx++) {
             if (picked[idx])
-                pair.add(array[idx]);
+                pair.add(originArr[idx]);
         }
         total.add(pair);
     }
 
-    public void recomb(int[] recombArr, int n, int r, int idx, int target) {
-        if (r == 0) {
-            for (int i = 0; i < recombArr.length; i++) {
-                System.out.print(recombArr[i]+" ");
-            }
-            System.out.println("");
+    private void duplicateCombination(int[] resultArray, int total, int toPick, int pickIdx, int arrIdx) {
+        if (toPick == 0) {
+            this.total.add(IntStream.of(resultArray).boxed().collect(Collectors.toList()));
             return;
         }
-        if (target == n) return;
+        if (arrIdx == total) return;
 
-        recombArr[idx] = target;
-        recomb(recombArr, n, r-1, idx+1, target);
-        recomb(recombArr, n, r, idx, target+1);
+        resultArray[pickIdx] = originArr[arrIdx];
+        duplicateCombination(resultArray, total, toPick-1, pickIdx+1, arrIdx);
+        duplicateCombination(resultArray, total, toPick, pickIdx, arrIdx+1);
+    }
+
+    private void combination(int[] resultArray, int total, int toPick, int pickIdx, int arrIdx) {
+        if (toPick == 0) {
+            this.total.add(IntStream.of(resultArray).boxed().collect(Collectors.toList()));
+            return;
+        }
+        if (arrIdx == total) return;
+
+        resultArray[pickIdx] = originArr[arrIdx];
+        combination(resultArray, total, toPick-1, pickIdx+1, arrIdx+1);
+        combination(resultArray, total, toPick, pickIdx, arrIdx+1);
     }
 }
