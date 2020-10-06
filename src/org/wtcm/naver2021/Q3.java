715ab7caf81b02894aed6cbbee92266305be9edf
sonjongyoung <sondahum@gmail.com>
2020-10-06 17:31:33 +0900

    solve 2021 naver problem Q3 but, need to fix with DP (as-is : dfs)

--- src/org/wtcm/naver2021/Q3.java
+++ src/org/wtcm/naver2021/Q3.java
@@ -1,37 +1,68 @@
 package org.wtcm.naver2021;
 
-import java.util.*;
+/* note. 성냥개비 K개로 만들 수 있는 디지털 시계 모양 숫자(중복가능)
+
+ * */
+
+import java.util.Arrays;
+import java.util.LinkedList;
 
 public class Q3 {
     static int[] numbers = {6, 2, 5, 5, 4, 5, 6, 3, 7, 6};
+    static int K;
+    static int answer;
 
     public static void main(String[] args) {
-        int k = 5; // 6,11,1;
-        System.out.println(solution(k));
+        int k = 20; // 6,11,1;
+        solution(k);
     }
 
-    static void sum_up_recursive(ArrayList<Integer> numbers, int target, ArrayList<Integer> partial) {
-        int s = 0;
-        for (int x: partial) s += x;
-        if (s == target)
-            System.out.println("sum("+ Arrays.toString(partial.toArray())+")="+target);
-        if (s >= target)
-            return;
-        for(int i=0;i<numbers.size();i++) {
-            ArrayList<Integer> remaining = new ArrayList<Integer>();
-            int n = numbers.get(i);
-            for (int j=i+1; j<numbers.size();j++) remaining.add(numbers.get(j));
-            ArrayList<Integer> partial_rec = new ArrayList<Integer>(partial);
-            partial_rec.add(n);
-            sum_up_recursive(remaining,target,partial_rec);
+    static void solution(int k) {
+        K = k;
+        for (int pick = 1; pick <= 9; pick++) {
+            if (K < 2 * pick) continue;
+//            int[] result = new int[pick];
+//            Arrays.fill(result, -1);
+//            combination(result, numbers, numbers.length, pick, 0, 0);
+            LinkedList<Integer> result = new LinkedList<>();
+            permutation(result, 10, pick);
         }
-    }
-    static void sum_up(ArrayList<Integer> numbers, int target) {
-        sum_up_recursive(numbers,target,new ArrayList<Integer>());
+        System.out.println(answer);
     }
 
-    static long solution(int k) {
+    static void combination(int[] resultArray, int[] srcArray, int total, int toPick, int srcIdx, int resultIdx) {
+        if (toPick == 0) {
+            if (Arrays.stream(resultArray).sum() == K) {
+                int cnt = resultArray.length;
+                int tmp = 1;
+                while (cnt > 0) {
+                    tmp *= cnt--;
+                }
+                answer += tmp;
+            }
+            return;
+        }
 
-        return 0;
+        if (srcIdx >= total) return;
+        resultArray[resultIdx] = srcArray[srcIdx];
+
+        combination(resultArray, srcArray, total, toPick - 1, srcIdx, resultIdx + 1);
+        combination(resultArray, srcArray, total, toPick, srcIdx + 1, resultIdx);
+    }
+
+    static void permutation(LinkedList<Integer> resultList, int total, int toPick) {
+        if (resultList.size() == toPick) {
+            if (resultList.get(0) == 0 && toPick != 1) return;
+            int sum = 0;
+            for (int idx : resultList) sum += numbers[idx];
+            if (sum == K) answer++;
+            return;
+        }
+
+        for (int srcIdx = 0; srcIdx < total; srcIdx++) {
+            resultList.add(srcIdx);
+            permutation(resultList, total, toPick);
+            resultList.removeLast();
+        }
     }
 }
