40201a4b0aa309f65a1a5261f012d516a5fcf126
sonjongyoung <sondahum@gmail.com>
2020-10-06 14:53:57 +0900

    add acmicpc 2631 problem

---
+++ src/org/wtcm/acmicpc/q2XXX/q2631/Main_JY.java
@@ -0,0 +1,144 @@
+package org.wtcm.acmicpc.q2XXX.q2631;
+
+import java.io.*;
+import java.util.InputMismatchException;
+
+public class Main_JY {
+    public static void main(String[] args) {
+        InputReader in = new InputReader(System.in);
+        OutputWriter out = new OutputWriter(System.out);
+
+        Task question = new Task();
+        question.solution(in, out);
+        out.close();
+    }
+}
+
+class Task {
+    void solution(InputReader in, OutputWriter out) {
+
+    }
+}
+
+class InputReader {
+    private InputStream stream;
+    private byte[] buf = new byte[1024];
+    private int curChar;
+    private int numChars;
+
+    public int read() {
+        if (numChars == -1) {
+            throw new InputMismatchException();
+        } else {
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException var2) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0) {
+                    return -1;
+                }
+            }
+            return buf[curChar++];
+        }
+    }
+
+    public boolean isSpaceChar(int c) {
+        return c == 32 || c == 10 || c == 13 || c == 9 || c == -1;
+    }
+
+    public InputReader(InputStream stream) {
+        this.stream = stream;
+    }
+
+    public String next() {
+        int c = this.read();
+        while (isSpaceChar(c)) {
+            c = this.read();
+        }
+        StringBuilder result = new StringBuilder();
+        result.appendCodePoint(c);
+        while (!isSpaceChar(c = this.read())) {
+            result.appendCodePoint(c);
+        }
+        return result.toString();
+    }
+
+    public int nextInt() {
+        int c = this.read();
+        while (isSpaceChar(c)) {
+            c = this.read();
+        }
+        byte sgn = 1;
+        if (c == 45) {
+            sgn = -1;
+            c = this.read();
+        }
+        int res = 0;
+        while (c >= 48 && c <= 57) {
+            res *= 10;
+            res += c - 48;
+            c = this.read();
+            if (isSpaceChar(c)) {
+                return res * sgn;
+            }
+        }
+        throw new InputMismatchException();
+    }
+
+    public String[] nextArray(int size) {
+        String[] ret = new String[size];
+        for (int i = 0; i < size; i++) {
+            ret[i] = this.next();
+        }
+        return ret;
+    }
+
+    public int[] nextIntArray(int size) {
+        int[] ret = new int[size];
+        for (int i = 0; i < size; i++) {
+            ret[i] = this.nextInt();
+        }
+        return ret;
+    }
+}
+
+class OutputWriter {
+    private final PrintWriter writer;
+
+    public OutputWriter(OutputStream outputStream) {
+        writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+    }
+
+    public OutputWriter(Writer writer) {
+        this.writer = new PrintWriter(writer);
+    }
+
+    public void print(int i) {
+        writer.print(i);
+    }
+
+    public void print(long i) {
+        writer.print(i);
+    }
+
+    public void print(Object... objects) {
+        int len = objects.length;
+        for (int i = 0; i < len; i++) {
+            if (i != 0) {
+                writer.print(' ');
+            }
+            writer.print(objects[i]);
+        }
+    }
+
+    public void println() {
+        writer.println();
+    }
+
+    public void close() {
+        writer.close();
+    }
+}
