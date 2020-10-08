58a06c7a15692be038cf55c248b90b81f454e669
sonjongyoung <sondahum@gmail.com>
2020-10-08 18:10:29 +0900

    go home2

---
+++ src/org/wtcm/kakao2019/candidatekey/CandidateKey_JY.java
@@ -0,0 +1,19 @@
+package org.wtcm.kakao2019.candidatekey;
+
+public class CandidateKey_JY {
+    /*
+        String[][] relation = {
+                {"100", "ryan", "music", "2"}
+                , {"200", "apeach", "math", "2"}
+                , {"300", "tube", "computer", "3"}
+                , {"400", "con", "computer", "4"}
+                , {"500", "muzi", "music", "3"}
+                , {"600", "apeach", "music", "2"}
+        };
+        */
+    public int solution(String[][] relation) {
+        int answer = 0;
+        return answer;
+    }
+
+}
