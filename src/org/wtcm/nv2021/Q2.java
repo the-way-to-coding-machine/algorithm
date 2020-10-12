794d2f24ccd52de51361d089f8abc0a402e4cda8
sonjongyoung <sondahum@gmail.com>
2020-10-12 17:55:59 +0900

    trying to solve nv 2021 problem 2

--- src/org/wtcm/nv2021/Q2.java
+++ src/org/wtcm/nv2021/Q2.java
@@ -1,11 +1,21 @@
 package org.wtcm.nv2021;
 
+import java.util.Arrays;
+
 public class Q2 {
     public static void main(String[] args) {
-        solution();
+        int[][] edges = {
+                {0, 2}, {2, 1}, {2, 4}
+                , {3, 5}, {5, 4}, {5, 7}
+                , {7, 6}, {6, 8}};
+        int n = 9;
+        Task question = new Task();
+        Arrays.stream(question.solution(edges, n)).forEach(System.out::println);
     }
 
-    static void solution() {
-
+    private static class Task {
+        int[] solution(int[][] edges, int n) {
+            return null;
+        }
     }
 }
