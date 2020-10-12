8c96de2675822f097e08726d32e059c00864404c
sonjongyoung <sondahum@gmail.com>
2020-10-12 10:28:55 +0900

    rename package

---
+++ src/org/wtcm/nv2021/Q1.java
@@ -0,0 +1,34 @@
+package org.wtcm.nv2021;
+
+public class Q1 {
+    public static void main(String[] args) {
+        int[] p = {5, 4, 7, 2, 0, 6};
+        int[] c = {4, 6, 4, 9, 2, 3};
+
+        System.out.println(solution(6, p, c));
+    }
+
+    static String solution(int n, int[] p, int[] c) {
+        int stock = 0;
+        int price = 100;
+        int total = 0;
+
+        for (int i = 0; i < n; i++) {
+            stock += p[i];
+            if (stock - c[i] < 0) {
+                if (price == 25) {
+                    n = i;
+                    break;
+                } else {
+                    price /= 2;
+                }
+            } else {
+                stock -= c[i];
+                total += c[i] * price;
+                price = 100;
+            }
+        }
+        total /= n;
+        return String.valueOf(total);
+    }
+}
---
+++ src/org/wtcm/nv2021/Q2.java
@@ -0,0 +1,11 @@
+package org.wtcm.nv2021;
+
+public class Q2 {
+    public static void main(String[] args) {
+        solution();
+    }
+
+    static void solution() {
+
+    }
+}
