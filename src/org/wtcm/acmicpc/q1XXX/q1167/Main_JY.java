5f127f022262dc48eccd62c32be379e33e410833
sonjongyoung <sondahum@gmail.com>
2020-10-14 18:07:36 +0900

    solve acmicpc 1167 problem

--- src/org/wtcm/acmicpc/q1XXX/q1167/Main_JY.java
+++ src/org/wtcm/acmicpc/q1XXX/q1167/Main_JY.java
@@ -1,106 +1,78 @@
 package org.wtcm.acmicpc.q1XXX.q1167;
 
 import java.io.*;
-import java.util.InputMismatchException;
+import java.util.*;
 
 public class Main_JY {
-    public static void main(String[] args) {
-        InputReader in = new InputReader(System.in);
+    public static void main(String[] args) throws IOException {
         OutputWriter out = new OutputWriter(System.out);
 
         Task question = new Task();
-        question.solution(in, out);
+        question.solution(out);
         out.close();
     }
 }
 
 class Task {
-    void solution(InputReader in, OutputWriter out) {
-    }
-}
+    int V;
+    ArrayList<int[]>[] tree;
+    void solution(OutputWriter out) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        V = Integer.parseInt(br.readLine());
+        tree = new ArrayList[V+1];
 
-class InputReader {
-    private InputStream stream;
-    private byte[] buf = new byte[1024];
-    private int curChar;
-    private int numChars;
+        for (int i = 1; i <= V; i++) tree[i] = new ArrayList<>();
 
-    public int read() {
-        if (numChars == -1) {
-            throw new InputMismatchException();
-        } else {
-            if (curChar >= numChars) {
-                curChar = 0;
-                try {
-                    numChars = stream.read(buf);
-                } catch (IOException var2) {
-                    throw new InputMismatchException();
-                }
-                if (numChars <= 0) {
-                    return -1;
-                }
-            }
-            return buf[curChar++];
-        }
-    }
+        for (int i = 0; i < V; i++) {
+            String[] line = br.readLine().split(" ");
+            int from = Integer.parseInt(line[0]);
+            for (int idx = 1; idx < line.length-1; idx += 2) {
+                int to = Integer.parseInt(line[idx]);
+                int length = Integer.parseInt(line[idx+1]);
 
-    public boolean isSpaceChar(int c) {
-        return c == 32 || c == 10 || c == 13 || c == 9 || c == -1;
-    }
-
-    public InputReader(InputStream stream) {
-        this.stream = stream;
-    }
-
-    public String next() {
-        int c = this.read();
-        while (isSpaceChar(c)) {
-            c = this.read();
-        }
-        StringBuilder result = new StringBuilder();
-        result.appendCodePoint(c);
-        while (!isSpaceChar(c = this.read())) {
-            result.appendCodePoint(c);
-        }
-        return result.toString();
-    }
-
-    public int nextInt() {
-        int c = this.read();
-        while (isSpaceChar(c)) {
-            c = this.read();
-        }
-        byte sgn = 1;
-        if (c == 45) {
-            sgn = -1;
-            c = this.read();
-        }
-        int res = 0;
-        while (c >= 48 && c <= 57) {
-            res *= 10;
-            res += c - 48;
-            c = this.read();
-            if (isSpaceChar(c)) {
-                return res * sgn;
+                tree[from].add(new int[] {to, -length});
             }
         }
-        throw new InputMismatchException();
+        int[] distance = dijkstra(1);
+        int min=Integer.MAX_VALUE, idx=0;
+        for (int i = 2; i < distance.length; i++) {
+            if (min > distance[i]) {
+                min = distance[i];
+                idx = i;
+            }
+        }
+        distance = dijkstra(idx);
+        min=Integer.MAX_VALUE;
+        for (int i = 1; i < distance.length; i++) {
+            if (min > distance[i]) {
+                min = distance[i];
+            }
+        }
+        out.print(-min);
     }
 
-    public String[] nextArray(int size) {
-        String[] ret = new String[size];
-        for (int i = 0; i < size; i++) {
-            ret[i] = this.next();
-        }
-        return ret;
-    }
+    int[] dijkstra(int start) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((v1, v2) -> v1[1] - v2[1]);
+        int[] distance = new int[V+1];
+        Arrays.fill(distance, Integer.MIN_VALUE);
 
-    public int[] nextIntArray(int size) {
-        int[] ret = new int[size];
-        for (int i = 0; i < size; i++) {
-            ret[i] = this.nextInt();
+        pq.add(new int[] {start, 0});
+        distance[start] = 0;
+
+        while (!pq.isEmpty()) {
+            int[] cur = pq.poll();
+
+            if (cur[1] > distance[cur[0]]) continue;
+
+            for (int[] next : tree[cur[0]]) {
+                int newLength = distance[cur[0]] + next[1];
+                if (distance[next[0]] < newLength) {
+                    distance[next[0]] = newLength;
+                    pq.add(new int[] { next[0], newLength});
+                }
+            }
         }
-        return ret;
+        return distance;
     }
 }
 
