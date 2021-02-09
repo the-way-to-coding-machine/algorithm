d50d35fda7dd35e27e07acf050f6702e8b7dc6b7
suy427 <sondahum@gmail.com>
2021-02-09 18:00:52 +0900

    solving

--- src/org/wtcm/acmicpc/q1XXX/q1949/Main_JY.java
+++ src/org/wtcm/acmicpc/q1XXX/q1949/Main_JY.java
@@ -1,32 +1,31 @@
 package org.wtcm.acmicpc.q1XXX.q1949;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 public class Main_JY {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static List<int[]>[] adjList;
+    static int[][] map;
+    static int[][] dp;
     static int N;
 
     public static void main(String[] args) throws IOException {
         N = Integer.parseInt(br.readLine());
-        int[] ns = new int[N];
+        int[] population = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
-        int i = 0;
-        for (String population : br.readLine().split(" "))
-            ns[i++] = Integer.parseInt(population);
-
-        adjList = new List[N+1];
-        for (i = 1; i <= N; i++)
-            adjList[i] = new ArrayList<>();
-
-        for (i = 0; i < N; i++) {
-            String[] input = br.readLine().split(" ");
-            adjList[Integer.parseInt(input[0])].add(new int[] {Integer.parseInt(input[1]), ns[i]});
+        map = new int[N+1][N+1];
+        dp = new int[N+1][N+1];
+        for (int i = 0; i < N; i++) {
+            int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            map[input[0]][input[1]] = map[input[1]][input[0]] = 1;
         }
 
+        dfs(1);
+
+        System.out.println(Math.max(dp[N][0], dp[N][1]));
+    }
+
+    static void dfs(int cur) {
 
     }
 }
