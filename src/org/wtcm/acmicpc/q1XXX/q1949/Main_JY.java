d785606e6e5c3a0c9189dbb51e35ef6415a8c6a1
suy427 <sondahum@gmail.com>
2021-02-10 01:05:46 +0900

    solving 1949 (tree dp)

--- src/org/wtcm/acmicpc/q1XXX/q1949/Main_JY.java
+++ src/org/wtcm/acmicpc/q1XXX/q1949/Main_JY.java
@@ -1,32 +1,46 @@
 package org.wtcm.acmicpc.q1XXX.q1949;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main_JY {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static int[][] map;
+    static List<Integer>[] map;
+    static boolean[] visited;
     static int[][] dp;
+    static int[] pop;
     static int N;
 
     public static void main(String[] args) throws IOException {
         N = Integer.parseInt(br.readLine());
-        int[] population = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-
-        map = new int[N+1][N+1];
+        map = new List[N+1];
+        visited = new boolean[N+1];
+        Arrays.fill(map, new ArrayList<>());
         dp = new int[N+1][N+1];
-        for (int i = 0; i < N; i++) {
+
+        pop = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        for (int i = 0; i < N-1; i++) {
             int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            map[input[0]][input[1]] = map[input[1]][input[0]] = 1;
+            map[input[0]].add(input[1]);
+            map[input[1]].add(input[0]);
         }
 
-        dfs(1);
-
-        System.out.println(Math.max(dp[N][0], dp[N][1]));
+        visited[1] = true;
+        traverse(1);
+        System.out.println(Math.max(dp[1][0], dp[1][1]));
     }
 
-    static void dfs(int cur) {
-
+    static void traverse(int town) {
+        for (int linkedTown : map[town]) {
+            if (visited[linkedTown]) continue;
+            visited[linkedTown] = true;
+            traverse(linkedTown);
+            dp[town][0] += dp[linkedTown][1];
+            dp[town][1] += Math.max(dp[linkedTown][0], dp[linkedTown][1]);
+        }
+        dp[town][0] += pop[town-1];
     }
 }
 
