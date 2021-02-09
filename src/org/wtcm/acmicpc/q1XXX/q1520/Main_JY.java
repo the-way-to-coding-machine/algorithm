fb2335ef15e3a02628e00adda52f166a39ef9220
suy427 <sondahum@gmail.com>
2021-02-09 17:30:44 +0900

    resolve acmicpc 1520(DP) no dfs!! DP!!

--- src/org/wtcm/acmicpc/q1XXX/q1520/Main_JY.java
+++ src/org/wtcm/acmicpc/q1XXX/q1520/Main_JY.java
@@ -1,50 +1,52 @@
 package org.wtcm.acmicpc.q1XXX.q1520;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Arrays;
 
 public class Main_JY {
-    static int[] xMove = {1,0,-1,0};
-    static int[] yMove = {0,1,0,-1};
-    static int [][] map, visited;
     static int N,M;
+    static int[][] map;
+    static int[][] dp;
+    static int[] xPos = {1,0,-1,0};
+    static int[] yPos = {0,1,0,-1};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String[] firstLine = br.readLine().split(" ");
-        M = Integer.parseInt(firstLine[0]);
-        N = Integer.parseInt(firstLine[1]);
+        N = Integer.parseInt(firstLine[0]);   M = Integer.parseInt(firstLine[1]);
+        map = new int[N][M];
+        dp = new int[N][M]; // dp[N][M] --> (N, M)을 지나서 도착하는 경우의 수. -1이 아니라면 이미 구해진거니까 그대로 쓰기만 하면됨.
+        for (int i = 0; i < N; i++) Arrays.fill(dp[i], -1);
 
-        map = new int[M][N];
-        visited = new int[M][N];
-        for (int i = 0; i < M; i++)
-            Arrays.fill(visited[i], -1);
-
-        for (int i = 0; i < M; i++)
-            map[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        for (int row = 0; row < N; row++)
+            map[row] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
 
         System.out.println(dfs(0,0));
     }
 
-    static int dfs(int y, int x) {
-        if (x == N-1 && y == M-1)
+    public static int dfs(int row, int col) {
+        if (row == N-1 && col == M - 1)
             return 1;
 
-        if (visited[y][x] == -1) { // note. caching을 신박하게 사용했네..
-            int cur = map[y][x];
-            visited[y][x] = 0;
+        if (dp[row][col] == -1) {
+            dp[row][col] = 0;
             for (int direction = 0; direction < 4; direction++) {
-                int nextX = x + xMove[direction];
-                int nextY = y + yMove[direction];
-                if (nextX >= N || nextX < 0 || nextY >= M || nextY < 0) continue;
-                if (cur <= map[nextY][nextX]) continue;
-                visited[y][x] += dfs(nextY, nextX);
+                int nextCol = col + xPos[direction];
+                int nextRow = row + yPos[direction];
+
+                if (!isIn(nextRow, nextCol)) continue;
+                if (map[row][col] <= map[nextRow][nextCol]) continue;
+
+                dp[row][col] += dfs(nextRow, nextCol);
             }
         }
-        return visited[y][x];
+
+        return dp[row][col];
+    }
+
+    public static boolean isIn(int row, int col) {
+        return (0 <= row && row < N) && (0 <= col && col < M);
     }
 }
