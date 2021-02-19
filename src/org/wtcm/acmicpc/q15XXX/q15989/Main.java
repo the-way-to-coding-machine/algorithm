6c0bea41f1331715c627e70111ff3fc28bd83bda
suy427 <sondahum@gmail.com>
2021-02-19 14:24:56 +0900

    solve acmicpc 15989 (DP)

--- src/org/wtcm/acmicpc/q15XXX/q15989/Main.java
+++ src/org/wtcm/acmicpc/q15XXX/q15989/Main.java
@@ -4,8 +4,9 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class Main {
-    static int[] dp;
+    static int[][] dp;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -17,10 +18,21 @@ public class Main {
             list.add(input);
             n = Math.max(n, input);
         }
-        dp = new int[n+1];
 
-        for (int input : list)
-            bw.write(dp[input]+"\n");
+        dp = new int[n+1][4];
+        dp[1][1] = 1;
+        dp[2][1] = 1; dp[2][2] = 1;
+        dp[3][1] = 1; dp[3][2] = 1; dp[3][3] = 1;
+        for (int num = 4; num <= n; num++) {
+            dp[num][1] = 1;
+            dp[num][2] = dp[num-2][1] + dp[num-2][2];
+            dp[num][3] = dp[num-3][1] + dp[num-3][2] + dp[num-3][3];
+        }
+
+        for (int input : list) {
+            int ans = dp[input][1]+dp[input][2]+dp[input][3];
+            bw.write(ans + "\n");
+        }
 
         bw.close();
     }
