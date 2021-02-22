77b4a14ba4e1b483e895bf84afdf0373b399428c
suy427 <sondahum@gmail.com>
2021-02-22 16:09:04 +0900

    solve acmicpc problems (DP - LIS)

--- src/org/wtcm/acmicpc/q2XXX/q2352/Main.java
+++ src/org/wtcm/acmicpc/q2XXX/q2352/Main.java
@@ -1,4 +1,4 @@
-package org.wtcm.acmicpc.q11XXX.q11722;
+package org.wtcm.acmicpc.q2XXX.q2352;
 
 import java.io.*;
 import java.util.Arrays;
@@ -12,15 +12,14 @@ public class Main {
         int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
         int[] lis = new int[n+1];
-        lis[0] = Integer.MIN_VALUE;
-
         int idx = 0;
+
         for (int cur = 0; cur < n; cur++) {
-            if (lis[idx] < -arr[cur])
-                lis[++idx] = -arr[cur];
+            if (arr[cur] > lis[idx])
+                lis[++idx] = arr[cur];
             else {
-                int position = binarySearch(idx, -arr[cur], lis);
-                lis[position] = -arr[cur];
+                int position = binarySearch(0, idx, arr[cur], lis);
+                lis[position] = arr[cur];
             }
         }
 
@@ -28,12 +27,13 @@ public class Main {
         bw.close();
     }
 
-    private static int binarySearch(int end, int target, int[] list) {
-        int mid, start = 0;
+    private static int binarySearch(int start, int end, int target, int[] lis) {
+        int mid;
 
         while (start < end) {
-            mid = (start + end) >> 1;
-            if (list[mid] < target) {
+            mid = (start+end) >> 1;
+
+            if (lis[mid] < target) {
                 start = mid+1;
             } else {
                 end = mid;
