371bbfea2f17f68d3a09eb8958199a527c09e826
Son <thswhdals15@naver.com>
2024-08-18 10:03:25 +0900

    Submultiple 문제 풀이

---
+++ src/org/wtcm/programmers/general/Submultiple.java
@@ -0,0 +1,16 @@
+package org.wtcm.programmers.general;
+
+// 정수 n을 입력받아 n의 약수를 모두 더한 값을 리턴하는 함수, solution을 완성해주세요.
+// https://school.programmers.co.kr/learn/courses/30/lessons/12928
+
+class Submultiple {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i = 1; i <= n; i++) {           // 약수를 확인하는 것이기 때문에 n의 절반까지만 반복문이 돌고 구하고 return할때 n을 마저 더해주는 식으로
+            if(n % i == 0) {                    // 코드를 짰다면 훨씬 효율적이었을 것.
+                answer += i;
+            }
+        }
+        return answer;
+    }
+}
