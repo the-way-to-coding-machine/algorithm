402f859fc544cf849aebdf2c38e8dcd09ce433f2
Son <thswhdals15@naver.com>
2024-08-24 15:48:07 +0900

    문제 풀이 (직사각형 별 찍기)

---
+++ src/org/wtcm/programmers/general/RectangleStar.java
@@ -0,0 +1,24 @@
+package org.wtcm.programmers.general;
+
+import java.util.Scanner;
+
+// 이 문제에는 표준 입력으로 두 개의 정수 n과 m이 주어집니다.
+// 별(*) 문자를 이용해 가로의 길이가 n, 세로의 길이가 m인 직사각형 형태를 출력해보세요.
+// https://school.programmers.co.kr/learn/courses/30/lessons/12969
+
+
+class RectangleStar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        for(int i = 0; i < b; i++) {
+            for(int j = 0; j < a; j++) {
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+
+    }
+}
